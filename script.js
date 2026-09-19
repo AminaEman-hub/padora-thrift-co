d26ba6681bc48a5a44b80af8c20c0ff282590b9d
AminaEman-hub <aminaeman280@gmailcom>
2026-09-18 18:21:59 -0700

    Initial Padora Thrift Co. website

---
+++ script.js
@@ -0,0 +1,16 @@
+function showDetails(productName) {
+    alert(
+        productName +
+        "\n\nPre-Loved Footwear" +
+        "\nCarefully selected and checked for quality." +
+        "\n\nMore product details and ordering options will be available soon."
+    );
+}
+
+
+function contactUs() {
+    alert(
+        "Thank you for visiting Padora Thrift Co.!\n\n" +
+        "For product details and orders, please contact us."
+    );
+}
